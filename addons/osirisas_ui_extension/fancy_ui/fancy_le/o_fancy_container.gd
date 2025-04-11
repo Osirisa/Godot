101ff14c4ed1d2a77a39c34baa1cf60b0864b8ec
Osirisa <osirisa.games@gmail.com>
2025-04-11 22:21:46 +0200

    added more functionality in fancy container

--- addons/osirisas_ui_extension/fancy_ui/fancy_le/o_fancy_container.gd
+++ addons/osirisas_ui_extension/fancy_ui/fancy_le/o_fancy_container.gd
@@ -2,6 +2,16 @@
 class_name OFancyContainer
 extends Container
 
+enum LabelPosition {
+	TOP_LEFT,
+	TOP_CENTER,
+	TOP_RIGHT,
+	BOTTOM_LEFT,
+	BOTTOM_CENTER,
+	BOTTOM_RIGHT
+}
+
+
 @export_group("Body Settings")
 @export var padding_top: int = 4:
 	set(value):
@@ -30,20 +40,81 @@ extends Container
 	set(value):
 		border_color = value
 		queue_redraw()
-@export var border_radius: int = 4:
+@export var border_radius_top_left: int = 4:
 	set(value):
-		border_radius = value
+		border_radius_top_left = value
 		queue_redraw()
+
+@export var border_radius_top_right: int = 4:
+	set(value):
+		border_radius_top_right = value
+		queue_redraw()
+
+@export var border_radius_bottom_right: int = 4:
+	set(value):
+		border_radius_bottom_right = value
+		queue_redraw()
+
+@export var border_radius_bottom_left: int = 4:
+	set(value):
+		border_radius_bottom_left = value
+		queue_redraw()
+
 @export var border_width: int = 1:
 	set(value):
 		border_width = value
 		queue_redraw()
+
+@export var draw_top: bool = true:
+	set(value):
+		draw_top = value
+		queue_redraw()
+@export var draw_bottom: bool = true:
+	set(value):
+		draw_bottom = value
+		queue_redraw()
+@export var draw_right: bool = true:
+	set(value):
+		draw_right = value
+		queue_redraw()
+@export var draw_left: bool = true:
+	set(value):
+		draw_left = value
+		queue_redraw()
+
+@export var mitred_top_left: bool = false:
+	set(value):
+		mitred_top_left = value
+		queue_redraw()
+
+@export var mitred_top_right: bool = false:
+	set(value):
+		mitred_top_right = value
+		queue_redraw()
+
+@export var mitred_bottom_right: bool = false:
+	set(value):
+		mitred_bottom_right = value
+		queue_redraw()
+
+@export var mitred_bottom_left: bool = false:
+	set(value):
+		mitred_bottom_left = value
+		queue_redraw()
+
 
 @export_group("Label")
 @export var font: Font = SystemFont.new():
 	set(value):
 		font = value
 		queue_redraw()
+
+@export var label_position: LabelPosition = LabelPosition.TOP_LEFT:
+	set(value):
+		label_position = value
+		queue_redraw()
+		queue_sort()
+
 @export var label_text: String = "label":
 	set(value):
 		label_text = value
@@ -98,54 +169,179 @@ func get_usable_rect() -> Rect2:
 	if not font:
 		return Rect2(Vector2.ZERO, size)
 
-	var label_height = int(font.get_ascent(label_font_size))
-	var label_offset_top = label_height / 2 + border_radius + border_width
-	var inner_pos = Vector2(padding_left, label_offset_top + padding_top)
-	var inner_size = Vector2(
+	var label_height := int(font.get_height(label_font_size))
+	var max_top_radius := max(border_radius_top_left, border_radius_top_right)
+	var max_bottom_radius := max(border_radius_bottom_left, border_radius_bottom_right)
+
+	var label_space_top := 0
+	var label_space_bottom := 0
+
+	match label_position:
+		LabelPosition.TOP_LEFT, LabelPosition.TOP_CENTER, LabelPosition.TOP_RIGHT:
+			label_space_top = label_height / 2 + max_top_radius + border_width
+		LabelPosition.BOTTOM_LEFT, LabelPosition.BOTTOM_CENTER, LabelPosition.BOTTOM_RIGHT:
+			label_space_bottom = label_height / 2 + max_bottom_radius + border_width
+
+	var inner_pos := Vector2(padding_left, padding_top + label_space_top)
+	var inner_size := Vector2(
 		size.x - (padding_left + padding_right),
-		size.y - (label_offset_top + padding_top + padding_bottom)
+		size.y - (padding_top + padding_bottom + label_space_top + label_space_bottom)
 	)
+
 	return Rect2(inner_pos, inner_size)
 
 
-
-
 func _draw() -> void:
-	var zero_rect = Rect2i(Vector2i.ZERO, size)
-	#var label_height := int(font.get_ascent(label_font_size) / 2)
-	var label_height = int(font.get_string_size(label_text.strip_edges(), -1, -1 ,label_font_size).y)
-	var label_width := int(font.get_string_size(label_text.strip_edges(), -1, -1 ,label_font_size).x)
+	if not font:
+		return
+
+	var rect = Rect2(Vector2.ZERO, size)
+	var label_size = font.get_string_size(label_text.strip_edges(), -1, -1, label_font_size)
+	var label_width = int(label_size.x)
+	var label_height = int(label_size.y)
+	var label_center_y = int(label_height / 2)
+	var label_baseline_y = font.get_ascent(label_font_size)
 	
-	draw_rect(zero_rect,background_color if background_color else Color(1,1,1,0), true)
+	var top_y: int
+	var bottom_y: int
+	var pos_label: bool
 	
-	# Top left Arc
-	draw_arc(Vector2i(border_radius + border_width, border_radius + label_height / 2), border_radius, PI, 3*PI/2,12, border_color, border_width, false)
+	match label_position:
+		LabelPosition.TOP_LEFT,LabelPosition.TOP_CENTER, LabelPosition.TOP_RIGHT:
+			top_y = label_center_y
+			bottom_y = size.y - border_width
+			pos_label = true
+		LabelPosition.BOTTOM_LEFT, LabelPosition.BOTTOM_CENTER, LabelPosition.BOTTOM_RIGHT:
+			top_y = border_width
+			bottom_y = size.y - label_center_y
+			pos_label = false
 	
-	# Top left - Label
-	if ((border_radius + border_width - 1) - (label_positioning_x - label_spacing + border_width) < 0):
-		draw_line(Vector2(border_radius + border_width - 1, label_height / 2), Vector2(label_positioning_x - label_spacing + border_width, label_height / 2), border_color, border_width, false)
+	# Hintergrund
+	draw_rect(rect, background_color, true)
+
+# --- Top Left Arc / Mitred ---
+	#var corner_top_left = Vector2(border_width, border_width + label_center_y)
+	var corner_top_left = Vector2(border_width, top_y)
+	if border_radius_top_left > 0:
+		if mitred_top_left:
+			draw_corner_beveled(corner_top_left, Vector2(1, 0), Vector2(0, 1), border_radius_top_left)
+		else:
+			draw_arc(corner_top_left + Vector2(border_radius_top_left, border_radius_top_left), border_radius_top_left, PI, 3 * PI / 2, 12, border_color, border_width, false)
 	
-	# Label
-	draw_string(font, Vector2(label_positioning_x + border_width, font.get_ascent(label_font_size)), label_text, -1, -1, label_font_size, label_color)
 	
-	# Label - Top Right
-	if ((border_width + label_positioning_x + label_width + label_spacing) - (size.x - border_radius - border_width + 1) < 0):
-		draw_line(Vector2(border_width + label_positioning_x + label_width + label_spacing, label_height / 2), Vector2(size.x - border_radius - border_width + 1, label_height / 2), border_color, border_width, false)
+	# --- Label ---
+	var label_pos := Vector2()
+	match label_position:
+		LabelPosition.TOP_LEFT:
+			label_pos = Vector2(label_positioning_x + border_width, label_baseline_y)
+		LabelPosition.TOP_CENTER:
+			label_pos = Vector2(size.x / 2 - label_width / 2, label_baseline_y)
+		LabelPosition.TOP_RIGHT:
+			label_pos = Vector2(size.x - label_width - label_positioning_x - border_width, label_baseline_y)
+		LabelPosition.BOTTOM_LEFT:
+			label_pos = Vector2(label_positioning_x + border_width, size.y - border_width - label_height + label_baseline_y)
+		LabelPosition.BOTTOM_CENTER:
+			label_pos = Vector2(size.x / 2 - label_width / 2, size.y - border_width - label_height + label_baseline_y)
+		LabelPosition.BOTTOM_RIGHT:
+			label_pos = Vector2(size.x - label_width - label_positioning_x - border_width, size.y - border_width - label_height + label_baseline_y)
 	
-	# Top Right Arc
-	draw_arc(Vector2i(size.x - border_radius - border_width, border_radius + label_height / 2), border_radius,  0 , - PI/2 , 12, border_color, border_width, false)
+	draw_string(font, label_pos, label_text, -1, -1, label_font_size, label_color)
 	
-	# Right | Top - Bottom
-	draw_line(Vector2(size.x - border_width, label_height / 2 + border_radius - 1), Vector2(size.x - border_width, size.y - border_radius - border_width + 1), border_color, border_width, false)
 	
-	# Bottom Right Arc
-	draw_arc(Vector2(size.x - border_width - border_radius, size.y - border_radius - border_width), border_radius,  0 , PI/2 , 12, border_color, border_width, false)
 	
-	# Bottom Right - Bottom Left
-	draw_line(Vector2(size.x - border_width - border_radius + 1, size.y - border_width), Vector2(border_radius + border_width - 1, size.y - border_width), border_color, border_width, false)
+	# --- Top Lines (split left and right of label) ---
+
+	if draw_top:
+		
+		var left_x = get_arc_offset(border_radius_top_left) + border_width
+		var right_x = size.x - get_arc_offset(border_radius_top_right) - border_width
+		
+		var label_left = label_pos.x - label_spacing
+		var label_right = label_pos.x + label_width + label_spacing
+		
+		if not pos_label:
+			draw_line(Vector2(left_x, top_y), Vector2(right_x, top_y), border_color, border_width, false)
+		else:
+			if label_left > left_x:
+				draw_line(Vector2(left_x, top_y), Vector2(label_left, top_y), border_color, border_width, false)
+			
+			if right_x > label_right:
+				draw_line(Vector2(label_right, top_y), Vector2(right_x, top_y), border_color, border_width, false)
+
+
+	# --- Top Right Arc / Mitred ---
+	var corner_top_right = Vector2(size.x - border_width, top_y)
+	#var corner_top_right = Vector2(size.x - border_width, border_width + label_center_y)
+	if border_radius_top_right > 0:
+		if mitred_top_right:
+			draw_corner_beveled(corner_top_right, Vector2(-1, 0), Vector2(0, 1), border_radius_top_right)
+		else:
+			draw_arc(corner_top_right + Vector2(-border_radius_top_right, border_radius_top_right), border_radius_top_right, 0, -PI / 2, 12, border_color, border_width, false)
+
+
+	# --- Right Line ---
+	if draw_right:
+		var top_offset = get_arc_offset(border_radius_top_right)
+		var bottom_offset = get_arc_offset(border_radius_bottom_right)
+		draw_line(
+			Vector2(size.x - border_width, top_offset + top_y),
+			Vector2(size.x - border_width, bottom_y - bottom_offset),
+			border_color, border_width, false
+		)
+
+
+	# --- Bottom Right Arc / Mitred ---
+	var corner_bottom_right = Vector2(size.x - border_width, bottom_y)
+	if border_radius_bottom_right > 0:
+		if mitred_bottom_right:
+			draw_corner_beveled(corner_bottom_right, Vector2(-1, 0), Vector2(0, -1), border_radius_bottom_right)
+		else:
+			draw_arc(corner_bottom_right + Vector2(-border_radius_bottom_right, -border_radius_bottom_right), border_radius_bottom_right, 0, PI / 2, 12, border_color, border_width, false)
+
+	# --- Bottom Line ---
+	if draw_bottom:
+		
+		var left_x = get_arc_offset(border_radius_bottom_left) + border_width
+		var right_x = size.x - get_arc_offset(border_radius_bottom_right) - border_width
+		
+		var label_bottom_y = size.y - int(font.get_height(label_font_size) / 2)
+		var label_left = label_pos.x - label_spacing
+		var label_right = label_pos.x + label_width + label_spacing
+		if pos_label:
+			draw_line(Vector2(right_x, bottom_y), Vector2(left_x, bottom_y), border_color, border_width, false)
+		else:
+			if label_left > left_x:
+				draw_line(Vector2(label_left, bottom_y), Vector2(left_x, bottom_y), border_color, border_width, false)
+			
+			if right_x > label_right:
+				draw_line(Vector2(right_x, bottom_y), Vector2(label_right, bottom_y), border_color, border_width, false)
 	
-	# Bottom Left Arc
-	draw_arc(Vector2(border_radius + border_width, size.y - border_radius - border_width), border_radius,  PI , PI/2 , 12, border_color, border_width, false)
 	
-	# Left | Bottom - Top
-	draw_line(Vector2( border_width, size.y - border_radius - border_width + 1), Vector2(border_width, border_radius + label_height / 2 - 1), border_color, border_width, false)
+	# --- Bottom Left Arc / Mitred ---
+	var corner_bottom_left = Vector2(border_width, bottom_y)
+	if border_radius_bottom_left > 0:
+		if mitred_bottom_left:
+			draw_corner_beveled(corner_bottom_left, Vector2(1, 0), Vector2(0, -1), border_radius_bottom_left)
+		else:
+			draw_arc(corner_bottom_left + Vector2(border_radius_bottom_left, -border_radius_bottom_left), border_radius_bottom_left, PI, PI / 2, 12, border_color, border_width, false)
+
+	# --- Left Line ---
+	if draw_left:
+		var top_offset = get_arc_offset(border_radius_top_left)
+		var bottom_offset = get_arc_offset(border_radius_bottom_left)
+		draw_line(
+			Vector2(border_width, top_y + top_offset),
+			Vector2(border_width, bottom_y - bottom_offset),
+			border_color, border_width, false
+		)
+
+
+func draw_corner_beveled(pos: Vector2, dir1: Vector2, dir2: Vector2, length: float) -> void:
+	var p1 = pos + dir1.normalized() * length
+	var p2 = pos + dir2.normalized() * length
+	draw_line(p1, p2, border_color, border_width, false)
+
+
+
+func get_arc_offset(radius: int) -> int:
+	return radius if radius > 0 else 0
